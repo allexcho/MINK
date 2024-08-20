cc1d047d7f5fa08b49ddf8a5318909bdca0feacf
Alex cho <allexcho98@gmail.com>
2024-08-19 23:30:32 -0300

    modificaciones de js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,97 +1,84 @@
-// Logica de login
-
-// Seleccionar el formulario 
-
+//logica del login
+//seleccionar el formulario
 const loginForm = document.querySelector("form");
 
-//  Agregar u evento para el envio del formulario en el storage
+//agregar u evento para el envio del formulario en el storage
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-loginForm.addEventListener("submit", function(event){
-    event.preventDefault(); 
+  //obtener los valores de los input
+  const email = document.querySelector('input[type="text"]').value;
+  const password = document.querySelector('input[type="password"]').value;
 
-    // Obtener los valores de los input
+  //obtener la lista de los usuarios que se almacenen
+  const users = JSON.parse(localStorage.getItem("users")) || [];
 
-    const email = document.querySelector("input[type="text"]").value;
+  //verificar si la contraseña existe y el usuario
+  const user = users.find(
+    (user) => user.email === email && user.password === password
+  );
 
-    const password = document.querySelector("input[type="password"]").value;
+  if (user) {
+    alert("inicio de seccion exitoso!");
 
-    // Obtener la lista de los usuarios qye se almacenen 
+    //redirigir al usuario a la pagina principal
+    window.location.href = "../index.html";
+  } else {
+    alert("Correo o contraseña incorrecta!!!");
+  }
+});
 
-    const users = JSON.parse(localStorage.getItem("users")) || []:
+//logica del registro
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector("form");
 
-    // Verificar si el usuario/contrasena existe 
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-    const user = users.find(users => user.email === email && user.password === password)
-    
-    if(user){
-        alert("Inicio Exitoso");
+    const name = form.querySelector(
+      'input[placeholder="Enter your name"]'
+    ).value;
+    const email = form.querySelector(
+      'input[placeholder="Enter your email"]'
+    ).value;
+    const password = form.querySelector(
+      'input[placeholder="Create password" ]'
+    ).value;
+    const confirmPassword = form.querySelector(
+      'input[ placeholder="Confirm password"]'
+    ).value;
+    const termsAccepted = form.querySelector('input[type="checkbox"]').checked;
 
-        // Redirigir al usuario a la pagina incial
-
-        window.location.href= "../index.html"
-
-    }else{
-        alert("Correo o contrasena incorrecta!")
+    //validar nuestro formulario
+    if (!name || !email || !password || !confirmPassword) {
+      alert("Por favor, completa todos los campos");
+      return;
     }
-})
 
-// Logica del registro
-
-document.addEventListener("DOMContentLoaded", function(){
-
-    const form = document.querySelector("form");
-
-    form.addEventListener("submit"), function(event){
-        event.preventDefault();
-
-        const name = form.querySelector("input[placerholder="Enter your name"]").value;
-
-        const email = form.querySelector("input[placeholder="Enter your email"]").value;
-
-        const password = form.querySelector("input[placeholder="Create password"]").value;
-
-        const confirmPassword = form.querySelector("input[placeholder="Confirm password"]").value;
-
-        const termsAccepted = form.querySelector("input[type="check"]").checked;
-
-        // Validar nuestro formulario
-
-        if(!name || !email || password || !confirmPassword){
-            alert("Porfavor, Completa todos los campos")
-            return;
-        }
-
-        if(password !== confirmPassword){
-            alert("Las contrasenas no coinciden, ingresa la correcta")
-            return;
-        }
-
-        if (!termsAccepted) {
-            alert("Para continuar, debes aceptar los terminos y condiciones.");
-            return
-        }
-
-        // Obtener los usuarios existentes del localStorage
-
-        const users = JSON.parse(localStorage.getItem("users")) || [];
-
-        // Verificar si ya esta registrado ese email
-
-        const userExists = users.some(user => user.email === email);
-
-        if(userExists){
-            alert("Este correo ya esta registrado");
-            return;
-        }
-
-        // Agregar el nuevo usuario 
-
-        users.push({name, email, password});
-        localStorage.setItem("users", JSON.stringify(users));
-        alert("Register Exitoso!");
-
-        // Redirigirlo al index
-
-        window.location.href = "../index.html"
+    if (password !== confirmPassword) {
+      alert("Las contraseñas no coinciden, ingresalas correctamente");
+      return;
     }
-})
+    if (!termsAccepted) {
+      alert("Para continuar, debes aceptar los terminos y condiciones.");
+      return;
+    }
+    //obtener los usuarios existentes del local
+    const users = JSON.parse(localStorage.getItem("users")) || [];
+
+    //verificasr si ya esta registrado ese email
+    const userExist = users.some((user) => user.email === email);
+    if (userExist) {
+      alert("Este correo ya esta registrado!!");
+      return;
+    }
+
+    //agregar el nuevo usuario
+    users.push({ name, email, password });
+    localStorage.setItem("users", JSON.stringify(users));
+    alert("Registro exitoso!!!");
+
+    //redirigirlo al index
+    window.location.href = "../index.html";
+  });
+});
